26717fc8d5e6e9363815a174731daf7dd79dde08
W. Nicholas Greene <wng@csail.mit.edu>
2020-10-27 20:02:25 -0400

    Add scripts to download example model/images.

---
+++ download_example.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget -O sample_data.pkl.tar.gz https://www.dropbox.com/s/hr59f24byc3x8z3/sample_data.pkl.tar.gz?dl=0
+tar -xzf sample_data.pkl.tar.gz
---
+++ download_model.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -O opensource_model.pth.tar https://www.dropbox.com/s/o1n1w0chlrw4lqt/opensource_model.pth.tar?dl=0
